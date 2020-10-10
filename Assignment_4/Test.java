2f55603e19b4ce51e0aef99f856df939b756448e
Aiden-Qian <69572923+Aiden-Qian@users.noreply.github.com>
2020-10-10 21:56:01 +0800

    Create Test.java

---
+++ Assignment_4/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {3,2,3,1,2,4,5,5,6};
+		int k=4;
+		System.out.println(Kth.findKthLargest(arr,k));
+  }
+}
