0314cbca7067a40a300f1e37897afd7bd43d22ce
Aiden-Qian <69572923+Aiden-Qian@users.noreply.github.com>
2020-10-27 17:08:59 +0800

    Create TestPet.java

---
+++ Assignment_6/PartC/TestPet.java
@@ -0,0 +1,11 @@
+public class TestPet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cat mimi=new Cat("Zara","ZiZi","BLACK",1,"Long");
+		Dog wawa=new Dog("Niky","ZaZa","WHITE",2,"Small");
+		System.out.println(mimi.toString());
+		System.out.println(wawa.toString());
+	}
+
+}
